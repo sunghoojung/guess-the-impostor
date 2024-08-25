d6410e0a5a3174b0f96c7ffa304332afda664168
Sunghoo Jung <77477503+sunghoojung@users.noreply.github.com>
2024-08-25 12:09:45 -0400

    Update footer.tsx

--- src/components/homePage/footer.tsx
+++ src/components/homePage/footer.tsx
@@ -6,7 +6,14 @@ interface FooterProps {
 const Footer: React.FC<FooterProps> = ({children}) => {
     return (
         <div className="bg-white absolute w-full h-[248px] left-0 top-[1450px] z-0">
-            
+            <div className="flex flex-row justify-between items-center pb-[3px]">
+                <div className="text-black font-inter text-[24px] font-normal leading-[150%] tracking-[0%] text-left">
+                    Guess the Impostor
+                </div>
+                <div className="text-[#454545] font-inter text-[16px] font-medium leading-[150%] tracking-[0%] text-right">
+                    Website designed by Sunghoo Jung
+                </div>
+            </div>
         </div>
     );
 }
